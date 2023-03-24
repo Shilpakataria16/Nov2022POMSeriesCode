4ebb10677e227da0e0cc5e4c752a99fc4bc07de2
Shilpa <shilpakataria16@gmail.com>
2023-03-24 11:39:17 -0500

    Mail -- Shilpa

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+	public void SendMail(){
+		System.out.println("mail sent--Shilpa");
+	}
+	
+	public void getMail() {
+		System.out.println("get mail --Shilpa");
+	}
+}
